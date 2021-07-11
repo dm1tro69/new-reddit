d4d2d44bc00e5f3920eb96cc6bcdbe288c7fcca0
dm1tro69 <dimolg22@gmail.com>
2021-07-11 10:59:17 +0300

    add less jest

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './header.less'
+
+const Header = () => {
+    return (
+        <header className={style.example}>
+            <h1>Reddit for our own</h1>
+        </header>
+    );
+};
+
+export default Header;
